a6bdad9671c09a735bf4e864443048d6b8ea3a7c
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2020-11-13 19:26:47 +0300

    ddasdasd

---
+++ TD_last/TD_last/EntityManager.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Trap.h"
+#include "Lier.h"
+#include "Castle.h"
+#include "Tower.h"
+#include "level.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Trap;
+class Lier;
+class Castle;
+class Tower;
+
+class EntityManager
+{
+public:
+	EntityManager();
+	bool setParameters(Level* lvl);
+	bool update(sf::Time frameTime);
+	void draw();
+private:
+	int  sortSimpleRoad(std::vector <Tile>);
+	std::vector<Trap> traps;
+	std::vector<Lier> liers;
+	std::vector <Castle> castles;
+	std::vector<Tower> towers;
+};
+
